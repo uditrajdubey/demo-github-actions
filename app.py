acfa1119452dbaea1505dda3d168795a39e72f89
Your Name <you@example.com>
2026-09-15 11:52:19 +0530

    added secrets to app file

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-secrets= "123@22"
-
+# FAKE TESTING CREDENTIALS TO TRIGGER BETTERLEAKS
+# AWS Rules require a 20-character identifier and a 40-character secret key
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
